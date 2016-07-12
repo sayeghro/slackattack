df4adf588a7de7f1683f62b08a2caff84c9df58c
Robert Sayegh <robert.sayegh@dali.dartmouth.edu>
2016-07-11 23:02:46 -0400

    added giphy api

--- app/server.js
+++ app/server.js
@@ -43,6 +43,22 @@ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 
   });
 });
 
+controller.hears(['gif', 'giphy', '.gif'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.startConversation(message, (err, convo) => {
+    convo.ask('What would you like to see in your gif?', (res) => {
+      giphy.search({
+        q: 'pokemon',
+        limit: 1,
+        rating: 'g',
+      }, (err, res) => {
+        convo.say(res);
+      });
+      convo.next();
+    });
+    convo.next();
+  });
+});
+
 controller.on('outgoing_webhook', (bot, message) => {
   bot.replyPublic(message, 'yeah yeah');
 });
